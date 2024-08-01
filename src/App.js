c6d3a022b13de3ae4827b92ef69eea42461459ed
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-08-01 14:12:51 -0300

    q

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ export default function App() {
   return (
     <header>
       <div className='opções'>
-          
+        três traços
       </div>
-      <div className='icons-e-opções'>
-
+      <div className='icons'>
+       <div id='exemplo1'>1</div>
+       <div id='exemplo2'>2</div>
+       <div id='exemplo3'>3</div>
       </div>
     </header>
   );
