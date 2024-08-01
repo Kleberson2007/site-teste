d4a0bd352ff64f16287c1753b40e17844fb30981
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-08-01 14:54:02 -0300

    a

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ export default function App() {
         três traços
       </div>
       <div className='icons'>
-       <div id='exemplo1'>1</div>
-       <div id='exemplo2'>2</div>
-       <div id='exemplo3'>3</div>
+        <div id='exemplo1'>1</div>
+        <div id='exemplo2'>2</div>
+        <div id='exemplo3'>3</div>
       </div>
     </header>
   );
