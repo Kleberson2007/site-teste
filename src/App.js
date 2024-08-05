1497db4ac9af33d41fbbf037c2868820b4052204
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-08-05 13:56:52 -0300

    A

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ export default function App() {
 
   return (
     <header>
-      <div className='opções'>
-        três traços
+      <div className='menu'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+        </svg>
+      </div>
+      <div className='pesquisar'>
+        <imput />
       </div>
       <div className='icons'>
         <div id='exemplo1'>1</div>
